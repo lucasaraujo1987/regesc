b23dec4a80c9717125f3c8697c2d2122366ba27e
lucasaraujo1987 <lukass.araujo11@gmail.com>
2022-12-16 21:37:50 -0300

    Update CrudProfessorService.java

--- src/main/java/br/com/xavecoding/regesc/service/CrudProfessorService.java
+++ src/main/java/br/com/xavecoding/regesc/service/CrudProfessorService.java
@@ -1,5 +1,6 @@
 package br.com.xavecoding.regesc.service;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
@@ -22,14 +23,22 @@ public class CrudProfessorService {
 			System.out.println("\nQual opção você quer executar?");
 			System.out.println("0 - Voltar ao menu anterior");
 			System.out.println("1 - Cadastrar novo Professor");
+			System.out.println("2 - Atualizar um Professor");
+			System.out.println("3 - Visualizar todos os Professores");
 			
 			int opcao = scanner.nextInt();
 			
 			switch (opcao) {
-			case 1: {
+			case 1: 
 				this.cadastrar(scanner);
 				break;	
-			}
+			
+			case 2:
+				this.atualizar(scanner);
+				
+			case 3:
+				this.visualizar(scanner);
+				
 			default:
 				isTrue = false;
 				break;
@@ -48,6 +57,40 @@ public class CrudProfessorService {
 		Professor professor = new Professor(nome, prontuario);
 		this.professorRepository.save(professor);
 		System.out.println("Professor salvo no Banco!!!\n");
+	}
+	
+	private void atualizar(Scanner scanner) {
+		System.out.print("Digite o id do professor a ser atualizado: ");
+		Long id = scanner.nextLong();
+		
+		Optional<Professor> optional = this.professorRepository.findById(id);
+		
+		if(optional.isPresent()) {
+			
+			System.out.print("Digite o nome do professor: ");
+			String nome = scanner.next();
+			
+			System.out.print("Digite o prontuario do professor: ");
+			String prontuario = scanner.next();
+			
+			Professor professor = optional.get();
+			professor.setNome(nome);
+			professor.setProntuario(prontuario);
+			
+			professorRepository.save(professor);
+			System.out.println("Professor atualizado com sucesso!!!\n");
+			
+			menu(scanner);
+			
+		}else {
+			System.out.println("O Id do professor informado: " + id + " é inválido\n");
+			
+			menu(scanner);
+		}
+	}
+	
+	private void visualizar() {
+		
 	}
 	
 }
